7b992a72ff6c9e0660d42424131201b4449554d6
夜 <414444599@qq.com>
2026-01-16 02:19:01 +0800

    Update index.ts

--- functions/index.ts
+++ functions/index.ts
@@ -1,3 +1,42 @@
-import { onRequestOptions, onRequest } from "./[[default]]";
+import { onRequestOptions, onRequest as originalOnRequest } from "./[[default]]";
+
+enum HttpMethod {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    PATCH = "PATCH",
+    HEAD = "HEAD",
+    OPTIONS = "OPTIONS"
+}
+
+const onRequest = async (request: Request) => {
+    const method = request.method as HttpMethod;
+    switch (method) {
+        case HttpMethod.GET:
+            console.log('Handling GET request');
+            break;
+        case HttpMethod.POST:
+            console.log('Handling POST request');
+            break;
+        case HttpMethod.PUT:
+            console.log('Handling PUT request');
+            break;
+        case HttpMethod.DELETE:
+            console.log('Handling DELETE request');
+            break;
+        case HttpMethod.PATCH:
+            console.log('Handling PATCH request');
+            break;
+        case HttpMethod.HEAD:
+            console.log('Handling HEAD request');
+            break;
+        case HttpMethod.OPTIONS:
+            return onRequestOptions(request);
+        default:
+            console.log('Unsupported request method');
+    }
+    return originalOnRequest(request);
+};
 
 export { onRequestOptions, onRequest };
